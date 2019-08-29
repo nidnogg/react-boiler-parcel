e1638ee0d8be477023d523f357632de8c6f1f621
nidnogg <henriquevt98@gmail.com>
2019-08-29 04:46:45 -0300

    Added initial structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './css/App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h3>
+        all I ever wanted
+      </h3>
+    </div>
+  );
+}
+
+export default App;
